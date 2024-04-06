7a94f272663bb48d94c58a4e15e74100f08b9e31
Dunn <leroy.c.dunn@gmail.com>
2024-04-06 20:43:37 +0200

    Revert "Update Program.cs - Bot Commands Occassionally Dropped in Multiplayer Matches"
    This reverts commit 2a3d66ade3336d809787dd0495ac4339f81accac.

--- Sproutopia/Program.cs
+++ Sproutopia/Program.cs
@@ -61,6 +61,7 @@ namespace Sproutopia
                         services.AddSingleton<GameState>();
                         services.AddSingleton<IBotManager, BotManager>();
                         services.AddSingleton<IGardenManager, GardenManager>();
+                        services.AddSingleton<RunnerHub>();
                         services.AddSignalR(options =>
                         {
                             //Uncomment for detailed error log for SignalR
